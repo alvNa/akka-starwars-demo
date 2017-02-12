7643c7858c297cea233f718b4ee044f7e2cbb3b9
alvNa <anavarro@datiobd.com>
2017-02-12 19:48:39 +0100

    Chage formats

--- src/main/scala/com/datio/akka/demo/Constants.scala
+++ src/main/scala/com/datio/akka/demo/Constants.scala
@@ -6,8 +6,6 @@ object Constants {
   val DESIGNER_KEY = "Designer"
   val MINER_KEY = "Miner"
   val WORKER_KEY = "Worker"
-
-  val DEATH_STAR = "DEATH STAR"
 }
 
 object Status extends Enumeration with Serializable {
--- src/main/scala/com/datio/akka/demo/actor/DesignerActor.scala
+++ src/main/scala/com/datio/akka/demo/actor/DesignerActor.scala
@@ -3,8 +3,6 @@ package com.datio.akka.demo.actor
 import akka.actor.{Actor, ActorLogging, Props}
 import com.datio.akka.demo.{RequestBuilding, ResponsePlans}
 
-
-
 object DesignerActor {
   def props(): Props = Props(new DesignerActor())
 }
@@ -17,7 +15,6 @@ class DesignerActor extends Actor with ActorLogging{
   def receive: Receive = {
     case _: RequestBuilding => handleRequest()
   }
-
 
   private def handleRequest(){
     log.info(s"${getClass.getName()} Designing plans ...")
